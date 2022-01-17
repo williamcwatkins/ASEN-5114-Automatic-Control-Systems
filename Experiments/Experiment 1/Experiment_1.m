68ec6c2525e1cacade32c75c9a6ddcc90ea688a6
williamcwatkins <william.watkins@colorado.edu>
2022-01-16 22:43:28 -0700

    Init

---
+++ Experiments/Experiment 1/Experiment_1.m
@@ -0,0 +1,11 @@
+%% Experiment 1
+% William Watkins
+% 16 January 2022
+
+%% Constants
+Rm = 19.2;      % [Ohms]
+Lm = 0.0019;    % [Henrys]
+Ktau = 1/40.1;  % [A/mNm]
+Kb = 1/238 / (2*pi/60); % [V/(rad/s)]
+Ngearhead = 10;
+Nmotor
