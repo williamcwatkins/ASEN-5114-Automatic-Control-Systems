7434c7915b329f7dde444d63794ada9bda3b916e
williamcwatkins <william.watkins@colorado.edu>
2022-02-10 09:22:05 -0700

    Exp 4

--- Experiments/Experiment 4/Code/Experiment_4.m
+++ Experiments/Experiment 4/Code/Experiment_4.m
@@ -8,3 +8,24 @@ close all
 clc
 
 %% Constants
+m1 = 20;
+m2 = 375;
+Kw = 1000000;
+Ks = 130000;
+B = 9800;
+Input = 1;
+tspan = 0:0.01:10;
+Initial = [0; 0; 0; 0];
+
+%% Settings
+options = odeset('AbsTol',1e-9,'RelTol',1e-9);
+
+%% Running ODE45
+[t, y] = ode45(@(t,y) Problem2_5(t,y,Input,m1,m2,Kw,Ks,B), tspan, Initial, options);
+plot(t,y);
+
+%% Try in LTI SS model
+A = [-(B/m1) (B/m1) (1/m1) -(1/m1);
+    (B/m2) -(B/m2) 0 -(1/m2);
+    Kw 0 0 0;
+    -Ks Ks 0 0];
---
+++ Experiments/Experiment 4/Code/Problem2_5.m
@@ -0,0 +1,13 @@
+function xDot = Problem2_5(t, x, u, m1, m2, Kw, Ks, B)
+%UNTITLED3 Summary of this function goes here
+%   Detailed explanation goes here
+Input = u;
+State = x;
+x1Dot = -(B/m1) * State(1) + (B/m1) * State(2) ...
+    + (1/m1) * State(3) - (1/m1) * State(4);
+x2Dot = (B/m2) * State(1) - (B/m2) * State(2) ...
+    - (1/m2) * State(4);
+x3Dot = Kw * State(1) - Kw * Input;
+x4Dot = -Ks * State(1) + Ks * State(2);
+xDot = [x1Dot; x2Dot; x3Dot; x4Dot];
+end
