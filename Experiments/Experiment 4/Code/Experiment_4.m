87f02ebc80b0da4ea5f6e1dc2b726d0f7c28302d
williamcwatkins <william.watkins@colorado.edu>
2022-02-17 20:21:41 -0700

    Exp 4

--- Experiments/Experiment 4/Code/Experiment_4.m
+++ Experiments/Experiment 4/Code/Experiment_4.m
@@ -8,24 +8,114 @@ close all
 clc
 
 %% Constants
-m1 = 20;
-m2 = 375;
-Kw = 1000000;
-Ks = 130000;
-B = 9800;
+m1 = 10;
+m2 = 350;
+Kw = 500000;
+Ks = 10000;
+b = 0;
 Input = 1;
-tspan = 0:0.01:10;
+tspan = 0:0.001:5;
 Initial = [0; 0; 0; 0];
 
-%% Settings
-options = odeset('AbsTol',1e-9,'RelTol',1e-9);
-
-%% Running ODE45
-[t, y] = ode45(@(t,y) Problem2_5(t,y,Input,m1,m2,Kw,Ks,B), tspan, Initial, options);
-plot(t,y);
-
 %% Try in LTI SS model
-A = [-(B/m1) (B/m1) (1/m1) -(1/m1);
-    (B/m2) -(B/m2) 0 -(1/m2);
+A = [-(b/m1) (b/m1) -(1/m1) (1/m1);
+    (b/m2) -(b/m2) 0 -(1/m2);
     Kw 0 0 0;
     -Ks Ks 0 0];
+
+B = [0;0;-Kw;0];
+C = [1 0 0 0;
+     0 1 0 0];
+D = [0;0];
+
+%% Simulating
+simOut = sim('Car_Suspension_Model.slx');
+output(1,:) = simOut.yout{1}.Values.Data(:,2)';
+tSpan(1,:) = simOut.yout{1}.Values.Time';
+output(2,:) = simOut.yout{2}.Values.Data(:,2)';
+tSpan(2,:) = simOut.yout{2}.Values.Time';
+Final = simOut.yout{3}.Values.Data(:,2)';
+tfinal = simOut.yout{3}.Values.Time';
+output(4,:) = simOut.yout{4}.Values.Data';
+tSpan(4,:) = simOut.yout{4}.Values.Time';
+
+figure('Position', [200 200 1000 800]);
+hold on;
+grid on;
+plot(tfinal,Final)
+
+b = 2000;
+A = [-(b/m1) (b/m1) -(1/m1) (1/m1);
+    (b/m2) -(b/m2) 0 -(1/m2);
+    Kw 0 0 0;
+    -Ks Ks 0 0];
+simOut = sim('Car_Suspension_Model.slx');
+Final = simOut.yout{3}.Values.Data(:,2)';
+tfinal = simOut.yout{3}.Values.Time';
+plot(tfinal,Final)
+
+b = 4000;
+A = [-(b/m1) (b/m1) -(1/m1) (1/m1);
+    (b/m2) -(b/m2) 0 -(1/m2);
+    Kw 0 0 0;
+    -Ks Ks 0 0];
+simOut = sim('Car_Suspension_Model.slx');
+Final = simOut.yout{3}.Values.Data(:,2)';
+tfinal = simOut.yout{3}.Values.Time';
+plot(tfinal,Final)
+
+b = 4000;
+A = [-(b/m1) (b/m1) -(1/m1) (1/m1);
+    (b/m2) -(b/m2) 0 -(1/m2);
+    Kw 0 0 0;
+    -Ks Ks 0 0];
+simOut = sim('Car_Suspension_Model.slx');
+Final = simOut.yout{3}.Values.Data(:,2)';
+tfinal = simOut.yout{3}.Values.Time';
+plot(tfinal,Final)
+
+b = 6000;
+A = [-(b/m1) (b/m1) -(1/m1) (1/m1);
+    (b/m2) -(b/m2) 0 -(1/m2);
+    Kw 0 0 0;
+    -Ks Ks 0 0];
+simOut = sim('Car_Suspension_Model.slx');
+Final = simOut.yout{3}.Values.Data(:,2)';
+tfinal = simOut.yout{3}.Values.Time';
+plot(tfinal,Final)
+
+b = 8000;
+A = [-(b/m1) (b/m1) -(1/m1) (1/m1);
+    (b/m2) -(b/m2) 0 -(1/m2);
+    Kw 0 0 0;
+    -Ks Ks 0 0];
+simOut = sim('Car_Suspension_Model.slx');
+Final = simOut.yout{3}.Values.Data(:,2)';
+tfinal = simOut.yout{3}.Values.Time';
+plot(tfinal,Final)
+
+b = 10000;
+A = [-(b/m1) (b/m1) -(1/m1) (1/m1);
+    (b/m2) -(b/m2) 0 -(1/m2);
+    Kw 0 0 0;
+    -Ks Ks 0 0];
+simOut = sim('Car_Suspension_Model.slx');
+Final = simOut.yout{3}.Values.Data(:,2)';
+tfinal = simOut.yout{3}.Values.Time';
+plot(tfinal,Final)
+
+b = 12000;
+A = [-(b/m1) (b/m1) -(1/m1) (1/m1);
+    (b/m2) -(b/m2) 0 -(1/m2);
+    Kw 0 0 0;
+    -Ks Ks 0 0];
+simOut = sim('Car_Suspension_Model.slx');
+Final = simOut.yout{3}.Values.Data(:,2)';
+tfinal = simOut.yout{3}.Values.Time';
+plot(tfinal,Final)
+
+legend({'b = 0','b=2,000','b=4,000','b=6,000','b=8,000','b=10,000'...
+    ,'b=12,000'},'Location','northeast')
+title('Car Body Acceleration in Response to Step Input')
+xlabel('Time [s]')
+ylabel('m/s^2')
--- Experiments/Experiment 4/Code/Problem2_5.m
+++ Experiments/Experiment 4/Code/Problem2_5.m
@@ -4,7 +4,7 @@ function xDot = Problem2_5(t, x, u, m1, m2, Kw, Ks, B)
 Input = u;
 State = x;
 x1Dot = -(B/m1) * State(1) + (B/m1) * State(2) ...
-    + (1/m1) * State(3) - (1/m1) * State(4);
+    - (1/m1) * State(3) + (1/m1) * State(4);
 x2Dot = (B/m2) * State(1) - (B/m2) * State(2) ...
     - (1/m2) * State(4);
 x3Dot = Kw * State(1) - Kw * Input;
